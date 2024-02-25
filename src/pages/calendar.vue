6a0533e9dbd9fcf6d6b7ea133ab48bf7753a864c
Vitor Somenzi <vitor.some@gmail.com>
2024-02-25 16:34:16 -0300

    Initial Load

---
+++ src/pages/calendar.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Calendar Page</h1>
+    <p>This is the calendar page</p>
+    <ClientOnly>
+        <DatePicker v-model="date"/>
+    </ClientOnly>
+
+</template>
+<script setup>
+    const DatePicker = defineAsyncComponent(async () => {
+        const { DatePicker } = await import('v-calendar');
+        import ('v-calendar/style.css');
+        return DatePicker;
+    });
+        
+    const date = ref(new Date());
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>LALALALALA</h1>
+</template>
